0de54ce866d31c2b0e0ccee4981176c23f18f13d
Hemant Sharma <hemantbeast@gmail.com>
2016-01-13 23:08:12 +0530

    msm: msm_rq_stats: Tone down mpdecision
    Add a new Kconfig to enable more conservative computations sent
    to mpdecision:
    
     * Do not make IO busy
     * Do a time window weighted load computation instead of just a sum
     * Do an sliding window averaged time computation for number runnable
    
    The purpose of these changes is to make mpdecision less likely to
    keep extra cores online.  It currently has a tendancy to run more
    cores running than needed and this can even cause some lag issues.
    
    Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
    Signed-off-by: LuffyXDA <beastdragon01@gmail.com>
    Signed-off-by: Hemant Sharma <hemantbeast@gmail.com>

--- kernel/sched/core.c
+++ kernel/sched/core.c
@@ -120,7 +120,7 @@ void start_bandwidth_timer(struct hrtimer *period_timer, ktime_t period)
 
 DEFINE_MUTEX(sched_domains_mutex);
 DEFINE_PER_CPU_SHARED_ALIGNED(struct rq, runqueues);
-#ifdef CONFIG_INTELLI_PLUG
+#if defined(CONFIG_INTELLI_PLUG) || defined(CONFIG_MSM_RUN_QUEUE_STATS_BE_CONSERVATIVE)
 DEFINE_PER_CPU_SHARED_ALIGNED(struct nr_stats_s, runqueue_stats);
 #endif
 
@@ -2438,7 +2438,7 @@ unsigned long nr_iowait(void)
 	return sum;
 }
 
-#ifdef CONFIG_INTELLI_PLUG
+#if defined(CONFIG_INTELLI_PLUG) || defined(CONFIG_MSM_RUN_QUEUE_STATS_BE_CONSERVATIVE)
 unsigned long avg_nr_running(void)
 {
 	unsigned long i, sum = 0;
