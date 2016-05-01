ac3d285a38d9ea0aca4f1937c82d81901739f832
hemantbeast <hemantbeast@gmail.com>
2016-05-01 10:43:10 +0530

    cpufreq: add smartmax_eps governor
    Signed-off-by: hemantbeast <hemantbeast@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -415,6 +415,9 @@ extern struct cpufreq_governor cpufreq_gov_intellimm;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX)
 extern struct cpufreq_governor cpufreq_gov_smartmax;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_smartmax)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_SMARTMAX_EPS)
+extern struct cpufreq_governor cpufreq_gov_smartmax_eps;
+#define CPUFREQ_DEFAULT_GOVERNOR        (&cpufreq_gov_smartmax_eps)
 #endif
 
 
